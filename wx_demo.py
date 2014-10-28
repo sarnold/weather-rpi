8a35c3729f6a3028e1f1bc16fdf4b45e8204eaaf
nll <nll@vctlabs.com>
2014-10-28 13:54:07 -0700

    getting started on display program for weather display on the Raspberry Pi, assuming PiTFT hardware.

---
+++ wx_demo.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+# display information on rpi (for weather demo)
+# using the pygame module
+
+import pygame as pyg
+import os, sys
+
+# globals section
+
+wpix=320   # display width in pixels
+dpix=250   # display depth in pixels
+
+
+def main():
+    # configuration section
+    # initialize pygame
+    # 
+    
+    # the event processing loop
+    # button check section
+    # data acquisition section
+    # screen update section
+    
+    pass
+
+
+
+
+
+#this calls the 'main' function when this script is executed
+if __name__ == '__main__':
+    main()
