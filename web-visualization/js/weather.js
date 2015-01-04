7c4ba251ebb8b9ef703463bb59f7a2ed90b06f4e
Donald Burr <dburr@borg-cube.com>
2015-01-03 18:06:50 -0700

    Parse json file directly, add min/max/last/trend
    - stores last read values and compares new values, displays
      arrows indicationg trend (greater/lesser/same)
    - keep track of min/max values (not displayed in ui yet)
    - persists all values (min/max/last) as cookies
    - parses wxdata.json file directly weirdo python dates

--- web-visualization/js/weather.js
+++ web-visualization/js/weather.js
@@ -39,14 +39,46 @@
 // Default configuration options
 var defaults = [
 ["time_format", "local", "form-time"],
-["temperature_units", "celsius", "form-temperature_units"],
-["refresh_interval", "60", "form-refresh_interval"]
-];
+  ["temperature_units", "celsius", "form-temperature_units"],
+  ["refresh_interval", "60", "form-refresh_interval"]
+  ];
 
-// Set to true to enable debugging output
-var DEBUG = false;
+  // keep track of stuff
+  var current_temperature = 0;
+  var last_temperature = 0;
+  var highest_temperature = 0;
+  var lowest_temperature = 0;
+  var highest_temperature_timestamp = "";
+  var lowest_temperature_timestamp = "";
 
-// debug logging
+  // keep track of stuff
+  var current_pressure = 0;
+  var last_pressure = 0;
+  var highest_pressure = 0;
+  var lowest_pressure = 0;
+  var highest_pressure_timestamp = "";
+  var lowest_pressure_timestamp = "";
+
+  // keep track of stuff
+  var current_humidity = 0;
+  var last_humidity = 0;
+  var highest_humidity = 0;
+  var lowest_humidity = 0;
+  var highest_humidity_timestamp = "";
+  var lowest_humidity_timestamp = "";
+
+  // keep track of stuff
+  var current_dewpoint = 0;
+  var last_dewpoint = 0;
+  var highest_dewpoint = 0;
+  var lowest_dewpoint = 0;
+  var highest_dewpoint_timestamp = "";
+  var lowest_dewpoint_timestamp = "";
+
+  // Set to true to enable debugging output
+  var DEBUG = true;
+
+  // debug logging
 function LOG(msg)
 {
   if (DEBUG)  {
@@ -58,6 +90,9 @@ function LOG(msg)
 $(document).ready(function(){
   // set up default options if not present
   setup_defaults();
+
+  // load historical data (if available)
+  load_historical_data();
 
   // set up clock to update every second
   setInterval('update_clock()', 1000);
@@ -75,7 +110,7 @@ $(document).ready(function(){
   // set some default content
   $( "#tab-historical" ).html("<p>We get signal!</p>");
   $( "#tab-windfield" ).html("<p>Main screen turn on!</p>");
-  
+
   // populate screen with initial data
   update_current_display();
 });
@@ -101,16 +136,16 @@ function setup_ui_elements()
       set_up_tab(theTab);
     }
   });
-  
+
   // set up dialogs
   $('#error-dialog').dialog({
     autoOpen: false,
     height: 140,
     modal: true,
     open: function(event, ui){
-     setTimeout("$('#error-dialog').dialog('close')",3000);
-   }
- });
+      setTimeout("$('#error-dialog').dialog('close')",3000);
+    }
+  });
   // set up settings accordion
   $('#settings_accordion').accordion({heightStyle: 'panel'});
 
@@ -130,35 +165,121 @@ function set_up_tab(tab)
 
 function setup_button_handlers()
 {
-  LOG("setup_button_handlers()");
-    // handle time set button
-    $("#button-save_settings").click(function (evt) {
-      update_settings();
-    });
-    $("#button-update").click(function(evt) {
-      force_update();
-    });
+  // handle time set button
+  $("#button-save_settings").click(function (evt) {
+    update_settings();
+  });
+  $("#button-update").click(function(evt) {
+    force_update();
+  });
+}
+
+function load_historical_data()
+{
+  last_temperature = $.cookie("last_temperature");
+  highest_temperature = $.cookie("highest_temperature");
+  lowest_temperature = $.cookie("lowest_temperature");
+  highest_temperature_timestamp = $.cookie("highest_temperature_timestamp");
+  lowest_temperature_timestamp = $.cookie("lowest_temperature_timestamp");
+  if (typeof last_temperature === "undefined")  {
+    last_temperature = 0;
+  }
+  if (typeof highest_temperature === "undefined")  {
+    highest_temperature = 0;
+  }
+  if (typeof lowest_temperature === "undefined")  {
+    lowest_temperature = 0;
+  }
+  if (typeof highest_temperature_timestamp === "undefined")  {
+    highest_temperature_timestamp = "";
+  }
+  if (typeof lowest_temperature_timestamp === "undefined")  {
+    lowest_temperature_timestamp = "";
   }
 
-  function setup_defaults()
-  {
-    LOG("setup_defaults()");
-    for (var i = 0; i < defaults.length; i++) {
-      var default_pair = defaults[i];
-      var setting_name = default_pair[0];
-      var setting_value = default_pair[1];
-      var current_value = get_saved_setting(setting_name);
-      LOG("current value for " + setting_name + " is: " + current_value);
-      if (typeof current_value === "undefined")  {
-        LOG("setting default = " + setting_value);
-        set_setting(setting_name, setting_value);
-      }
+  last_pressure = $.cookie("last_pressure");
+  highest_pressure = $.cookie("highest_pressure");
+  lowest_pressure = $.cookie("lowest_pressure");
+  highest_pressure_timestamp = $.cookie("highest_pressure_timestamp");
+  lowest_pressure_timestamp = $.cookie("lowest_pressure_timestamp");
+  if (typeof last_pressure === "undefined")  {
+    last_pressure = 0;
+  }
+  if (typeof highest_pressure === "undefined")  {
+    highest_pressure = 0;
+  }
+  if (typeof lowest_pressure === "undefined")  {
+    lowest_pressure = 0;
+  }
+  if (typeof highest_pressure_timestamp === "undefined")  {
+    highest_pressure_timestamp = "";
+  }
+  if (typeof lowest_pressure_timestamp === "undefined")  {
+    lowest_pressure_timestamp = "";
+  }
+
+  last_humidity = $.cookie("last_humidity");
+  highest_humidity = $.cookie("highest_humidity");
+  lowest_humidity = $.cookie("lowest_humidity");
+  highest_humidity_timestamp = $.cookie("highest_humidity_timestamp");
+  lowest_humidity_timestamp = $.cookie("lowest_humidity_timestamp");
+  if (typeof last_humidity === "undefined")  {
+    last_humidity = 0;
+  }
+  if (typeof highest_humidity === "undefined")  {
+    highest_humidity = 0;
+  }
+  if (typeof lowest_humidity === "undefined")  {
+    lowest_humidity = 0;
+  }
+  if (typeof highest_humidity_timestamp === "undefined")  {
+    highest_humidity_timestamp = "";
+  }
+  if (typeof lowest_humidity_timestamp === "undefined")  {
+    lowest_humidity_timestamp = "";
+  }
+
+  last_dewpoint = $.cookie("last_dewpoint");
+  highest_dewpoint = $.cookie("highest_dewpoint");
+  lowest_dewpoint = $.cookie("lowest_dewpoint");
+  highest_dewpoint_timestamp = $.cookie("highest_dewpoint_timestamp");
+  lowest_dewpoint_timestamp = $.cookie("lowest_dewpoint_timestamp");
+  if (typeof last_dewpoint === "undefined")  {
+    last_dewpoint = 0;
+  }
+  if (typeof highest_dewpoint === "undefined")  {
+    highest_dewpoint = 0;
+  }
+  if (typeof lowest_dewpoint === "undefined")  {
+    lowest_dewpoint = 0;
+  }
+  if (typeof highest_dewpoint_timestamp === "undefined")  {
+    highest_dewpoint_timestamp = "";
+  }
+  if (typeof lowest_dewpoint_timestamp === "undefined")  {
+    lowest_dewpoint_timestamp = "";
+  }
+}
+
+function setup_defaults()
+{
+  LOG("setup_defaults()");
+  for (var i = 0; i < defaults.length; i++) {
+    var default_pair = defaults[i];
+    var setting_name = default_pair[0];
+    var setting_value = default_pair[1];
+    var current_value = get_saved_setting(setting_name);
+    LOG("current value for " + setting_name + " is: " + current_value);
+    if (typeof current_value === "undefined")  {
+      LOG("setting default = " + setting_value);
+      set_setting(setting_name, setting_value);
     }
   }
+}
 
-  function update_settings_ui()
-  {
-    LOG("update_settings_ui()");
+function update_settings_ui()
+{
+  LOG("update_settings_ui()");
   // time_format
   // temperature_units
   // refresh_interval
@@ -192,7 +313,7 @@ function update_settings()
   if (update_interval > 0)  {
     window.update_current_display_process = setTimeout(update_current_display, update_interval * 1000);
   }
-  
+
   $('#save_success').show().delay(1000).fadeOut('slow');
   set_unit_labels();
 }
@@ -230,7 +351,7 @@ function get_date_string(d)
     w = d.getDay();
     s = weekdays[w] + ", " + d.toLocaleString();
   }
-  
+
   return s;
 }
 
@@ -272,12 +393,12 @@ function wind_tab_selected()
 function update_current_display()
 {
   force_update(function() {
-      // Schedule the next request when the current one's complete
-      var update_interval = get_saved_setting("refresh_interval");
-      if (update_interval > 0)  {
-        window.update_current_display_process = setTimeout(update_current_display, update_interval * 1000);
-      }
-    });
+    // Schedule the next request when the current one's complete
+    var update_interval = get_saved_setting("refresh_interval");
+    if (update_interval > 0)  {
+      window.update_current_display_process = setTimeout(update_current_display, update_interval * 1000);
+    }
+  });
 }
 
 function convert_to_current_temperature_unit(deg_celsius)
@@ -297,71 +418,203 @@ function convert_to_current_temperature_unit(deg_celsius)
 
 function force_update(completion_handler)
 {
-  LOG("UPDATING");
-  
+  LOG("UPDATING (new-style)");
+
   // disable the refresh button and replace it with a spinner
   toggleButton("#button-update");
-  
-  var jqxhr = $.ajax( "cgi-bin/get_current_readings.py" )
-  .done(function(data) {
+
+  var jqxhr = $.getJSON( "wxdata2.json", function(data) {
+    console.log( "success" );
     LOG("SUCCESS, received data: " + JSON.stringify(data, null, 4));
-    $("#field_temperature").text(convert_to_current_temperature_unit(data.temperature).toFixed(2));
-    $("#field_pressure").text(data.pressure);
-    $("#field_humidity").text(data.humidity.toFixed(2));
-    $("#field_dewpoint").text(data.dewpoint.toFixed(2));
-    
-    switch(data.temperature_trend)  {
-      case -1: $("#trend_temperature").html("&#8595;"); break;
-      case 0: $("#trend_temperature").html("&#8213;"); break;
-      case 1: $("#trend_temperature").html("&#8593;"); break;
-    }
-
-    switch(data.pressure_trend)  {
-      case -1: $("#trend_pressure").html("&#8595;"); break;
-      case 0: $("#trend_pressure").html("&#8213;"); break;
-      case 1: $("#trend_pressure").html("&#8593;"); break;
-    }
-
-    switch(data.humidity_trend)  {
-      case -1: $("#trend_humidity").html("&#8595;"); break;
-      case 0: $("#trend_humidity").html("&#8213;"); break;
-      case 1: $("#trend_humidity").html("&#8593;"); break;
-    }
-
-    switch(data.dewpoint_trend)  {
-      case -1: $("#trend_dewpoint").html("&#8595;"); break;
-      case 0: $("#trend_dewpoint").html("&#8213;"); break;
-      case 1: $("#trend_dewpoint").html("&#8593;"); break;
-    }
-    
-    var update_date = new Date(data.timestamp);
-    $("#field_last_updated").html(get_date_string(update_date));
+    process_observations(data);
   })
-.fail(function(error) {
-  LOG("FAILED, error details: " + JSON.stringify(error, null, 4));
-  // window.alert("Error loading data: " + error.status + " " + error.statusText);
-  $("#error-text").text("Error loading data: " + error.status + " " + error.statusText);
-  $("#error-dialog").dialog("open");
-})
-.always(function() {
-  // restore the update button
-  toggleButton("#button-update");
-  if (typeof completion_handler === "function") {
-    LOG("calling completion handler");
-    completion_handler();
-  }
-});
+  .fail(function(error) {
+    LOG("FAILED, error details: " + JSON.stringify(error, null, 4));
+    // window.alert("Error loading data: " + error.status + " " + error.statusText);
+    $("#error-text").text("Error loading data: " + error.status + " " + error.statusText);
+    $("#error-dialog").dialog("open");
+  })
+  .always(function() {
+    // restore the update button
+    toggleButton("#button-update");
+    if (typeof completion_handler === "function") {
+      LOG("calling completion handler");
+      completion_handler();
+    }
+  });
 }
 
-function toggleButton ( el ) {
- 
- if ( $(el).button( 'option', 'disabled' ) ) {
-  $(el).button( 'enable' );
-  $(el).button( 'option', 'label', 'Update' );
-  $(el).button( 'option', 'icons', { primary: null } );
-} else {
-  $(el).button( 'disable' );
-  $(el).button( 'option', 'label', 'Updating..' );
-  $(el).button( 'option', 'icons', { primary: 'ui-icon-waiting' } );
+function process_observations(data)
+{
+  LOG(data);
+  // first decode all the things!!
+  var version = data["ver-tag"];
+  var temperature;
+  var pressure;
+  var humidity;
+  var dewpoint;
+  var datetime;
+
+  if (version == 1) {
+    LOG("version 1 data detected");
+    datetime = data["time-tag"];
+    temperature = parseFloat(data["bmp-temp"]);
+    pressure = parseInt(data["bmp-pres"]);
+    humidity = parseFloat(data["sht-hum"]);
+    dewpoint = parseFloat(data["sht-dew"]);
+  }
+
+  $("#field_temperature").text(convert_to_current_temperature_unit(temperature).toFixed(2));
+  $("#field_pressure").text(pressure);
+  $("#field_humidity").text(humidity.toFixed(2));
+  $("#field_dewpoint").text(dewpoint.toFixed(2));
+
+  // now compute trends
+  temperature_trend = (temperature > last_temperature ? 1 : (temperature < last_temperature ? -1 : 0));
+  pressure_trend = (pressure > last_pressure ? 1 : (pressure < last_pressure ? -1 : 0));
+  humidity_trend = (humidity > last_humidity ? 1 : (humidity < last_humidity ? -1 : 0));
+  dewpoint_trend = (dewpoint > last_dewpoint ? 1 : (dewpoint < last_dewpoint ? -1 : 0));
+
+  // display it
+  switch(temperature_trend)  {
+    case -1: $("#trend_temperature").html("&#8595;"); break;
+    case 0: $("#trend_temperature").html("&#8213;"); break;
+    case 1: $("#trend_temperature").html("&#8593;"); break;
+  }
+
+  switch(pressure_trend)  {
+    case -1: $("#trend_pressure").html("&#8595;"); break;
+    case 0: $("#trend_pressure").html("&#8213;"); break;
+    case 1: $("#trend_pressure").html("&#8593;"); break;
+  }
+
+  switch(humidity_trend)  {
+    case -1: $("#trend_humidity").html("&#8595;"); break;
+    case 0: $("#trend_humidity").html("&#8213;"); break;
+    case 1: $("#trend_humidity").html("&#8593;"); break;
+  }
+
+  switch(dewpoint_trend)  {
+    case -1: $("#trend_dewpoint").html("&#8595;"); break;
+    case 0: $("#trend_dewpoint").html("&#8213;"); break;
+    case 1: $("#trend_dewpoint").html("&#8593;"); break;
+  }
+
+  // display update date
+  // example: 2014-12-31 01:52:32.414871
+  // docs: https://code.google.com/p/datejs/wiki/FormatSpecifiers
+  // Date.parseExact doesn't like the millisecond part of python date, so just strip
+  // it out.  Oh well, so much for millisecond-level accuracy. :-P
+  datetime = datetime.substring(0, datetime.indexOf('.'));
+  var update_date = Date.parseExact(datetime, "yyyy-MM-dd hh:mm:ss");
+  LOG(typeof datetime);
+  LOG(datetime);
+  LOG(typeof update_date);
+  LOG(update_date);
+  $("#field_last_updated").html(get_date_string(update_date));
+
+  // now store the last-values
+  last_temperature = temperature;
+  last_pressure = pressure;
+  last_dewpoint = dewpoint;
+  last_humidity = humidity;
+
+  $.cookie("last_temperature", last_temperature);
+  $.cookie("last_pressure", last_pressure);
+  $.cookie("last_humidity", last_humidity);
+  $.cookie("last_dewpoint", last_dewpoint);
+
+  // now handle min/max
+  if (temperature < lowest_temperature)  {
+    lowest_temperature = temperature;
+    lowest_temperature_timestamp = update_date;
+  }
+  if (temperature > highest_temperature)  {
+    highest_temperature = temperature;
+    highest_temperature_timestamp = update_date;
+  }
+
+  if (pressure < lowest_pressure)  {
+    lowest_pressure = pressure;
+    lowest_pressure_timestamp = update_date;
+  }
+  if (pressure > highest_pressure)  {
+    highest_pressure = pressure;
+    highest_pressure_timestamp = update_date;
+  }
+
+  if (humidity < lowest_humidity)  {
+    lowest_humidity = humidity;
+    lowest_humidity_timestamp = update_date;
+  }
+  if (humidity > highest_humidity)  {
+    highest_humidity = humidity;
+    highest_humidity_timestamp = update_date;
+  }
+
+  if (dewpoint < lowest_dewpoint)  {
+    lowest_dewpoint = dewpoint;
+    lowest_dewpoint_timestamp = update_date;
+  }
+  if (dewpoint > highest_dewpoint)  {
+    highest_dewpoint = dewpoint;
+    highest_dewpoint_timestamp = update_date;
+  }
+
+  // now save them out
+  $.cookie("lowest_temperature", lowest_temperature);
+  $.cookie("highest_temperature", highest_temperature);
+  $.cookie("lowest_temperature_timestamp", lowest_temperature_timestamp);
+  $.cookie("highest_temperature_timestamp", highest_temperature_timestamp);
+
+  $.cookie("lowest_pressure", lowest_pressure);
+  $.cookie("highest_pressure", highest_pressure);
+  $.cookie("lowest_pressure_timestamp", lowest_pressure_timestamp);
+  $.cookie("highest_pressure_timestamp", highest_pressure_timestamp);
+
+  $.cookie("lowest_humidity", lowest_humidity);
+  $.cookie("highest_humidity", highest_humidity);
+  $.cookie("lowest_humidity_timestamp", lowest_humidity_timestamp);
+  $.cookie("highest_humidity_timestamp", highest_humidity_timestamp);
+
+  $.cookie("lowest_dewpoint", lowest_dewpoint);
+  $.cookie("highest_dewpoint", highest_dewpoint);
+  $.cookie("lowest_dewpoint_timestamp", lowest_dewpoint_timestamp);
+  $.cookie("highest_dewpoint_timestamp", highest_dewpoint_timestamp);
+
 }
+
+function force_update_old(completion_handler)
+{
+  LOG("UPDATING zzz");
+
+  // disable the refresh button and replace it with a spinner
+  toggleButton("#button-update");
+
+  var jqxhr = $.ajax("wxdata.json" /*"cgi-bin/get_current_readings.py"*/ )
+    .done(function(data) {
+      LOG("SUCCESS, received data: " + JSON.stringify(data, null, 4));
+      process_observations(data);
+    })
+  .fail(function(error) {
+    LOG("FAILED, error details: " + JSON.stringify(error, null, 4));
+    // window.alert("Error loading data: " + error.status + " " + error.statusText);
+    $("#error-text").text("Error loading data: " + error.status + " " + error.statusText);
+    $("#error-dialog").dialog("open");
+  })
+  .always(function() {
+  });
+}
+
+function toggleButton ( el )
+{
+  if ( $(el).button( 'option', 'disabled' ) ) {
+    $(el).button( 'enable' );
+    $(el).button( 'option', 'label', 'Update' );
+    $(el).button( 'option', 'icons', { primary: null } );
+  } else {
+    $(el).button( 'disable' );
+    $(el).button( 'option', 'label', 'Updating..' );
+    $(el).button( 'option', 'icons', { primary: 'ui-icon-waiting' } );
+  }
 }
